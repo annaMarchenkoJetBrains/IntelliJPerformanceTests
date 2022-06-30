1b78d7a9aed154a8ac34c74f5b261884031e0431
Anna Koehler <anna.marchenko@jetbrains.com>
2022-06-30 14:16:30 +0200

    support actualized changes in ide starter

--- src/main/kotlin/data/IntelliJCases.kt
+++ src/main/kotlin/data/IntelliJCases.kt
@@ -1,13 +1,13 @@
 package data
 
 import com.intellij.ide.starter.data.TestCaseTemplate
-import com.intellij.ide.starter.models.IdeProduct
+import com.intellij.ide.starter.ide.IdeProductProvider
 import com.intellij.ide.starter.project.ProjectInfo
 
 import kotlin.io.path.div
 
 
-object IntelliJCases : TestCaseTemplate(IdeProduct.IC) {
+object IntelliJCases : TestCaseTemplate(IdeProductProvider.IC) {
     val CommunitySources = getTemplate().withProject(
         ProjectInfo(
             testProjectURL = "https://github.com/JetBrains/intellij-community/archive/master.zip",
